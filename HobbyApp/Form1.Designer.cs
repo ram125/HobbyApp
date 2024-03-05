b5fca33b893afa930cd965fd7fd25a9bed9aa9e9
r.dain24 <r.dain24@LAB108PC12>
2024-03-05 15:07:55 +0200

    final version

--- HobbyApp/Form1.Designer.cs
+++ HobbyApp/Form1.Designer.cs
@@ -30,36 +30,39 @@
         {
             comboBox1 = new ComboBox();
             comboBox2 = new ComboBox();
-            checkedListBox1 = new CheckedListBox();
             groupBox1 = new GroupBox();
             radioButton2 = new RadioButton();
             radioButton1 = new RadioButton();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            groupBox2 = new GroupBox();
+            checkBox3 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            button3 = new Button();
             groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Bulgaria", "Italy", "United States", "Netherlands" });
             comboBox1.Location = new Point(60, 21);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(182, 33);
             comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
             // 
             // comboBox2
             // 
             comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(60, 175);
+            comboBox2.Location = new Point(60, 132);
             comboBox2.Name = "comboBox2";
             comboBox2.Size = new Size(182, 33);
             comboBox2.TabIndex = 1;
-            // 
-            // checkedListBox1
-            // 
-            checkedListBox1.FormattingEnabled = true;
-            checkedListBox1.Location = new Point(298, 21);
-            checkedListBox1.Name = "checkedListBox1";
-            checkedListBox1.Size = new Size(180, 144);
-            checkedListBox1.TabIndex = 2;
             // 
             // groupBox1
             // 
@@ -75,12 +78,12 @@
             // radioButton2
             // 
             radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(21, 87);
+            radioButton2.Location = new Point(16, 74);
             radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(141, 29);
+            radioButton2.Size = new Size(99, 29);
             radioButton2.TabIndex = 1;
             radioButton2.TabStop = true;
-            radioButton2.Text = "radioButton2";
+            radioButton2.Text = "evening";
             radioButton2.UseVisualStyleBackColor = true;
             // 
             // radioButton1
@@ -88,25 +91,125 @@
             radioButton1.AutoSize = true;
             radioButton1.Location = new Point(16, 39);
             radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(141, 29);
+            radioButton1.Size = new Size(105, 29);
             radioButton1.TabIndex = 0;
             radioButton1.TabStop = true;
-            radioButton1.Text = "radioButton1";
+            radioButton1.Text = "morning";
             radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(158, 237);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(180, 129);
+            listBox1.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 25;
+            listBox2.Location = new Point(474, 237);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(180, 129);
+            listBox2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(377, 254);
+            button1.Name = "button1";
+            button1.Size = new Size(58, 34);
+            button1.TabIndex = 7;
+            button1.Text = ">>";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(377, 312);
+            button2.Name = "button2";
+            button2.Size = new Size(58, 34);
+            button2.TabIndex = 8;
+            button2.Text = "<<";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(checkBox3);
+            groupBox2.Controls.Add(checkBox2);
+            groupBox2.Controls.Add(checkBox1);
+            groupBox2.Location = new Point(282, 21);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(209, 150);
+            groupBox2.TabIndex = 9;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(16, 106);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(103, 29);
+            checkBox3.TabIndex = 2;
+            checkBox3.Text = "Hobby3";
+            checkBox3.UseVisualStyleBackColor = true;
+            checkBox3.CheckedChanged += checkBox3_CheckedChanged;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(19, 70);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(103, 29);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "Hobby2";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(20, 34);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(103, 29);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Hobby1";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(358, 404);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 10;
+            button3.Text = "select";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(groupBox2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
             Controls.Add(groupBox1);
-            Controls.Add(checkedListBox1);
             Controls.Add(comboBox2);
             Controls.Add(comboBox1);
             Name = "Form1";
             Text = "Form1";
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -114,9 +217,17 @@
 
         private ComboBox comboBox1;
         private ComboBox comboBox2;
-        private CheckedListBox checkedListBox1;
         private GroupBox groupBox1;
         private RadioButton radioButton2;
         private RadioButton radioButton1;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Button button1;
+        private Button button2;
+        private GroupBox groupBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private Button button3;
     }
 }
--- HobbyApp/Form1.cs
+++ HobbyApp/Form1.cs
@@ -5,8 +5,151 @@ namespace HobbyApp
         public Form1()
         {
             InitializeComponent();
-            string[] contents = { "hobbyroman", "hobbytoshko" };
-            checkedListBox1.DataSource = contents;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                listBox1.Items.Add(checkBox1.Text);
+            }
+            else
+            {
+                listBox1.Items.Remove(checkBox1.Text);
+            }
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                listBox1.Items.Add(checkBox2.Text);
+            }
+            else
+            {
+                listBox1.Items.Remove(checkBox2.Text);
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked == true)
+            {
+                listBox1.Items.Add(checkBox3.Text);
+            }
+            else
+            {
+                listBox1.Items.Remove(checkBox3.Text);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == 0)
+            {
+                listBox2.Items.Add(listBox1.Items[0].ToString());
+                listBox1.Items.RemoveAt(0);
+            }
+            else if (listBox1.SelectedIndex == 1)
+            {
+                listBox2.Items.Add(listBox1.Items[1].ToString());
+                listBox1.Items.RemoveAt(1);
+            }
+            else if (listBox1.SelectedIndex == 2)
+            {
+                listBox2.Items.Add(listBox1.Items[2].ToString());
+                listBox1.Items.RemoveAt(2);
+            }
+            else if (listBox1.SelectedIndex == 3)
+            {
+                listBox2.Items.Add(listBox1.Items[3].ToString());
+                listBox1.Items.RemoveAt(3);
+            }
+            else if (listBox1.SelectedIndex == 4)
+            {
+                listBox2.Items.Add(listBox1.Items[4].ToString());
+                listBox1.Items.RemoveAt(4);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex == 0)
+            {
+                listBox1.Items.Add(listBox2.Items[0].ToString());
+                listBox2.Items.RemoveAt(0);
+            }
+            else if (listBox2.SelectedIndex == 1)
+            {
+                listBox1.Items.Add(listBox2.Items[1].ToString());
+                listBox2.Items.RemoveAt(1);
+            }
+            else if (listBox1.SelectedIndex == 2)
+            {
+                listBox1.Items.Add(listBox2.Items[2].ToString());
+                listBox2.Items.RemoveAt(2);
+            }
+            else if (listBox1.SelectedIndex == 3)
+            {
+                listBox1.Items.Add(listBox2.Items[3].ToString());
+                listBox2.Items.RemoveAt(3);
+            }
+            else if (listBox1.SelectedIndex == 4)
+            {
+                listBox1.Items.Add(listBox2.Items[4].ToString());
+                listBox2.Items.RemoveAt(4);
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            if (comboBox1.SelectedIndex == 0)
+            {
+                comboBox2.Items.Add("Sofia");
+                comboBox2.Items.Add("Plovdiv");
+                comboBox2.Items.Add("Burgas");
+                comboBox2.Items.Add("Varna");
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                comboBox2.Items.Add("Milan");
+                comboBox2.Items.Add("Rome");
+                comboBox2.Items.Add("Bari");
+                comboBox2.Items.Add("Venice");
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                comboBox2.Items.Add("New York City");
+                comboBox2.Items.Add("Los Angeles");
+                comboBox2.Items.Add("Washington DC");
+                comboBox2.Items.Add("Philadelphia");
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                comboBox2.Items.Add("Amsterdam");
+                comboBox2.Items.Add("Rotterdam");
+                comboBox2.Items.Add("Leeuwarden");
+                comboBox2.Items.Add("Hague");
+            }
+        }
+        public string Time()
+        {
+            if (radioButton1.Checked)
+            {
+                return radioButton1.Text;
+            }
+            else
+            {
+                return radioButton2.Text;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string selectedHobbies = string.Join(", ", listBox2.Items.OfType<object>().Select(item => item.ToString()));
+            string selectedTime = Time();
+
+            MessageBox.Show($"Your chosen country is {comboBox1.SelectedItem.ToString()} and your chosen city is " +
+                $"{comboBox2.SelectedItem.ToString()}. Your chosen hobbies are {selectedHobbies}, and you prefer to do them in the {selectedTime}.");
+            Environment.Exit(0);
         }
     }
 }
